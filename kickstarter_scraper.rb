2772304e1d200777c95657ebb9a7bbb4e41d62b0
Doug Jones <dsjr1284@gmail.com>
2020-08-10 23:14:23 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,6 +1,14 @@
 require "nokogiri"
 require "pry"
 
+#create_project_hash
+# projects: kickstarter.css("li.project.grid_4")
+# title: project.css("h2.bbcard_name strong a").text
+# image link: project.css("div.project-thumbnail a img").attribute("src").value
+# description: project.css("p.bbcard_blurb").text
+# location: project.css("span.location-name").text
+# percent_funded: project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
+
 def create_project_hash
   html = File.read('fixtures/kickstarter.html')
   kickstarter = Nokogiri::HTML(html)
@@ -9,15 +17,30 @@ def create_project_hash
 
   # Iterate through the projects
   kickstarter.css("li.project.grid_4").each do |project|
-  title = project.css("h2.bbcard_name strong a").text
-  projects[title.to_sym] = {}
+    title = project.css("h2.bbcard_name strong a").text
+  projects[title.to_sym] = {
+    :image_link => project.css("div.project-thumbnail a img").attribute("src").value,
+    :description => project.css("p.bbcard_blurb").text,
+    :location => project.css("ul.project-meta span.location-name").text,
+    :percent_funded => project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
+  }
 end
+#
+# kickstarter.css("li.project.grid_4").each do |project|
+#    title = project.css("h2.bbcard_name strong a").text
+#    projects[title.to_sym] = {
+#      :image_link => project.css("div.project-thumbnail a img").attribute("src").value,
+#      :description => project.css("p.bbcard_blurb").text,
+#      :location => project.css("ul.project-meta span.location-name").text,
+#      :percent_funded => project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
+#    }
+
 
 # return the projects hash
  projects
 end
 
-create_project_hash
+
 # projects: kickstarter.css("li.project.grid_4")
 # title: project.css("h2.bbcard_name strong a").text
 # image link: project.css("div.project-thumbnail a img").attribute("src").value
